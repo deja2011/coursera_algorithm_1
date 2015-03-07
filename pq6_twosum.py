ec9a72c6aadbfcc300f264f6cdf5fb7093e70560
Lawrence Li <deja2011@163.com>
2015-03-07 20:23:18 +0800

    Finished Programming Question #6.

--- pq6_twosum.py
+++ pq6_twosum.py
@@ -1,39 +1,70 @@
 __author__ = 'Lawrence Li'
 
-
-def sortmethod(array):
-    array.sort()
+import multiprocessing
 
 
-def selectmethod(array):
-    length = len(array)
-    ret = list()
-    for ii in range(length):
-        left = array[ii]
-        for jj in range(ii + 1, length):
-            total = left + array[jj]
-            if -10000 <= total <= 10000:
-                ret.append(total)
-                break
-        print '\r%d' % int(float(ii) / 20001 * 100),
-    return len(set(ret))
+class CalTwoSum(multiprocessing.Process):
+
+    def __init__(self, begin, end, array):
+        multiprocessing.Process.__init__(self)
+        self.iter = xrange(begin, end + 1)
+        self.array = array
+        self.cnt = 0
+
+    def run(self):
+        is2sum = self.is2sum
+        for t in self.iter:
+            if is2sum(t):
+                self.cnt += 1
+
+    def is2sum(self, t):
+        s = self.array
+        for ele in s:
+            diff = t - ele
+            if diff != ele and diff in s:
+                return True
+        else:
+            return False
 
 
 def hashmethod(array):
     cnt = 0
-    for i in xrange(-10000, 10001):
-        for ele in array:
-            diff = i - ele
-            if diff != ele and diff in array:
-                cnt += 1
-                break
-        print '\r%d' % int(float(i) / 20001 * 100),
-    print cnt
+    for t in xrange(-10000, 10001):
+        if is2sum(t, array):
+            cnt += 1
+        print '\r%d' % t,
+    return cnt
 
 
 def main():
-    method = sortmethod
-    print method([int(ln[:-1]) for ln in open('algo1_programming_prob_2sum.txt', 'r')])
+
+    start, end, np = -10000, 10000, 10
+    array = set([int(ln[:-1]) for ln in open('algo1_programming_prob_2sum.txt', 'r')])
+
+    unit = abs(start - end) / np
+    starts = [(start + n * unit) for n in range(np)]
+    ends = [(start + n * unit + unit - 1) for n in range(np)]
+    ends[-1] = end + 2
+
+    plist = list()
+    cnt = 0
+    # print zip(starts, ends)
+
+    for s, e in zip(starts, ends):
+        p = CalTwoSum(s, e, array)
+        plist.append(p)
+
+    for p in plist:
+        p.start()
+
+    for p in plist:
+        p.join()
+        cnt += p.cnt
+
+    print cnt
+
+    # print len(array)
+    # print method([int(ln[:-1]) for ln in open('algo1_programming_prob_2sum.txt', 'r')])
 
 
 if __name__ == '__main__':
